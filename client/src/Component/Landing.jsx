305baabae0472844a3171b82e7302f007ddac56d
hasnat0006 <yusufrezahasnat0006@gmail.com>
2024-12-24 19:43:00 +0600

    Landing start

--- client/src/Component/Landing.jsx
+++ client/src/Component/Landing.jsx
@@ -1,16 +1,13 @@
 
 
 import Navbar from "./Navbar";
+import Hero from "./Hero";
 
 const Landing = () => {
     return (
         <div>
             <Navbar />
-            <div className="flex flex-col justify-center items-center tracking-wider mt-20 gap-2">
-                <p className="text-3xl text-center font-Poppins font-normal ">Achieve your dream Career with</p>
-                <p className="text-center text-4xl font-Poppins font-semibold tracking-wider text-CG">CAREER CLIMB</p>
-                <p className="text-center text-lg font-Poppins font-normal tracking-normal">Your career is your responsibility. Take charge of it with clarity and purpose.</p>
-            </div>
+            <Hero />
         </div>
     );
 }
--- client/src/Component/Navbar.jsx
+++ client/src/Component/Navbar.jsx
@@ -1,25 +1,29 @@
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
 
 const Navbar = () => {
-    return (
-        <nav className="flex font-Poppins justify-between items-center w-auto h-10 ml-10 mr-10">
-            <h1 className="text-xl font-semibold tracking-widest">CAREER CLIMB</h1>
-            <ul className="flex justify-center items-center space-x-5 mt-2">
-                <li className="ml-5 cursor-pointer hover:border-b-2 hover:border-y-zinc-950 hover:font-medium">Home</li>
-                <li className="ml-5 cursor-pointer hover:border-b-2 hover: border-y-zinc-950 hover:font-medium">Jobs/Internship</li>
-                <li className="ml-5 cursor-pointer hover:border-b-2 hover: border-y-zinc-950 hover:font-medium">Roadmap</li>
-                <li className="ml-5 cursor-pointer hover:border-b-2 hover: border-y-zinc-950 hover:font-medium">Skill Gap Analysis</li>
-            </ul>
-            <div className="flex justify-center items-center space-x-5">
-                <button className="relative font-Poppins z-10 px-3 h-auto py-1 bg-black rounded-md font-normal text-l text-white shadow-lg transition-all duration-250 overflow-hidden group hover:shadow-xl hover:bg-white hover:text-black">
-                    Log In
-                </button>
-                <button className="relative font-Poppins z-10 px-3 h-auto py-1 bg-black rounded-md font-normal text-l text-white shadow-lg transition-all duration-250 overflow-hidden group hover:shadow-xl hover:bg-white hover:text-black">
-                    Sign Up
-                </button>
-            </div>
-        </nav>
-    );
+  return (
+    <nav className="flex font-Poppins justify-between items-center w-full h-12 px-10 bg-white bg-opacity-10 backdrop-blur-md shadow-md sticky top-0 z-50">
+      <h1 className="text-xl font-semibold tracking-wider">CAREER CLIMB</h1>
+      <ul className="flex justify-center items-center space-x-5 mt-0">
+        <li className="ml-5 cursor-pointer hover:border-b-2 hover:border-y-zinc-950 hover:font-medium">Home</li>
+        <li className="ml-5 cursor-pointer hover:border-b-2 hover: border-y-zinc-950 hover:font-medium">Jobs/Internship</li>
+        <li className="ml-5 cursor-pointer hover:border-b-2 hover: border-y-zinc-950 hover:font-medium">Roadmap</li>
+        <li className="ml-5 cursor-pointer hover:border-b-2 hover: border-y-zinc-950 hover:font-medium">Skill Gap Analysis</li>
+      </ul>
+      <div className="flex justify-center items-center space-x-5">
+        <button className="flex justify-center items-center space-x-2  px-3 py-1 bg-black rounded-md font-normal text-sm text-white shadow-lg transition-all duration-250 overflow-hidden group hover:shadow-xl hover:bg-white hover:text-black ">
+          <text> Log In </text>
+          <FontAwesomeIcon icon={faArrowRight} />
+        </button>
+        <button className="flex justify-center items-center space-x-2  px-3 py-1 bg-black rounded-md font-normal text-sm text-white shadow-lg transition-all duration-250 overflow-hidden group hover:shadow-xl hover:bg-white hover:text-black">
+          <text> Sign Up </text>
+          <FontAwesomeIcon icon={faArrowRight} />
+        </button>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
