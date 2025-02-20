553cf599c8028ed17a0ff1c78722784d669bdbdc
hasnat0006 <yusufrezahasnat0006@gmail.com>
2025-02-20 23:18:21 +0600

    Refactor JobPortal and UserProfile components for improved readability and functionality

--- client/src/Component/Jobportal.jsx
+++ client/src/Component/Jobportal.jsx
@@ -22,21 +22,6 @@ function JobPortal() {
   );
 
   const handleApply = async (uuid) => {
-    // toast.success("Profile Updated", {
-    //   style: {
-    //     backgroundColor: "rgb(195, 232, 195)", // Sets background to green
-    //     color: "black", // Sets text color to white
-    //     fontWeight: "bold",
-    //   },
-    //   position: "bottom-center",
-    //   autoClose: 3000,
-    //   hideProgressBar: true,
-    //   closeOnClick: true,
-    //   pauseOnHover: true,
-    //   draggable: true,
-    //   progress: undefined,
-    // });
-
     try {
       const response = await fetch(`${url}/checkcv/${useruuid}`);
       if (!response.ok) console.log("Couldn't get");
@@ -166,6 +151,10 @@ function JobPortal() {
     };
     getallJobs();
   }, []);
+
+
+  console.log("Selected Job", selectedJob);
+
   return (
     <div>
       {/* Navbar Section */}
@@ -213,12 +202,16 @@ function JobPortal() {
                           {job.role}
                         </span>
                         <span className="bg-green-50 text-green-700 px-2 py-1 rounded">
-                          {job.salary} 
+                          {job.salary}
                         </span>
                       </div>
-                      <p>
-                        <strong>Minimum Requirement:</strong> <br />
-                        {job.required_skill}
+                      <p className="text-sm text-muted-foregroun py-2">
+                        {job.required_skill.map((skill, index) => (
+                          <span key={index} className="bg-gray-200 px-2 py-1 rounded mr-2">
+                            {skill}
+                          </span>
+                        ))}
+
                       </p>
                       <p className="text-sm text-muted-foreground line-clamp-2">
                         {job.description}
@@ -244,9 +237,9 @@ function JobPortal() {
             <div className="w-3/5 border-red-500 p-5 bg-gray-50 rounded-lg sticky max-h-[115vh] overflow-y-auto  shadow-lg">
               <div className=" flex justify-center items-center  py-2">
                 <img
-                  src={selectedJob.logo}
+                  src={selectedJob.company_logo}
                   alt={`${selectedJob.company_name} logo`}
-                  className="flex-grow-12 h-12 mb-2"
+                  className="flex-grow-12 h-32 mb-2 rounded-lg"
                 />
               </div>
 
@@ -256,12 +249,17 @@ function JobPortal() {
               <h3 className="flex justify-center items-center font-semibold text-lg mb-2">
                 {selectedJob.company_name} , {selectedJob.location}
               </h3>
-              <h3 className="text-bold"> Salary </h3>
-              <p className="text-gray-800 font-semibold space-y-4">
-                {selectedJob.salary}
-              </p>
-              <h3 className="text-lg font-bold mt-2"> Required Skills </h3>
-              <p className="text-gray-700 mt-2">{selectedJob.required_skill}</p>
+              <span className="text-bold font-semibold"> Salary:<br />
+              </span>
+              <span className="bg-green-50 text-green-700 space-y-4">
+                ${selectedJob.salary}
+              </span>
+              <h3 className="text-lg font-bold my-3"> Required Skills </h3>
+              {selectedJob.required_skill.map((skill, index) => (
+                <span key={index} className="bg-gray-200 px-2 py-1 rounded mr-2">
+                  {skill}
+                </span>
+              ))}
 
               <h3 className="text-lg font-bold mt-2">About Job </h3>
               <p className="text-gray-700 mt-4">{selectedJob.description}</p>
